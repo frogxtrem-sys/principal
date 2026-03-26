9836751050fde09a21e37128a078abf5c7fee93d
frogxtrem-sys <frogxtrem@gmail.com>
2026-03-26 14:47:08 -0300

    Update 2main.py

--- 2main.py
+++ 2main.py
@@ -1347,8 +1347,7 @@ def main():
                 continue
 
         elif setup_type == "2":
-            try:
-                auto_inject_logins() 
+            try: 
             # --------------------------------------------------------------
                 print("\033[1;32m[ Shouko.dev ] - Auto Setup User IDs from appStorage.json...\033[0m")
                 packages = ["ywcw.lnu.exhl", "ub.wnjb.bzz", "ixq.vf.jlr", "srl.mvn.gv", "kxm.ak.qyfi"]

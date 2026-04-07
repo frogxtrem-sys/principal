236dc80c7b1701fd5fa3d5ec3499338e76fd5b66
frogxtrem-sys <frogxtrem@gmail.com>
2026-04-06 22:53:33 -0300

    Update 2main.py

--- 2main.py
+++ 2main.py
@@ -206,7 +206,7 @@ workspace_paths = [f"{base_path}Workspace" for base_path in executors.values()] 
 # Adicione os caminhos reais dos dados dos clones
 clones_internos = ["ywcw.lnu.exhl", "ub.wnjb.bzz", "ixq.vf.jlr", "srl.mvn.gv", "kxm.ak.qyfi", "tk.lisa.cqt", 
                    "jpaclone.anya.lh", "jpaclone.anya.li", "jpaclone.anya.lj", "com.roblox.clienu", "com.roblox.clienv",
-                   "com.roblox.clienw", "com.roblox.clienx"
+                   "com.roblox.clienw", "com.roblox.clienx", "xtv.popp.kls", "kpl.mlst.nkl", "ply.oudj.tys", "lpo.mksr.wer"
 ]
 for pkg in clones_internos:
     workspace_paths.append(f"/data/data/{pkg}/files/workspace")
@@ -597,7 +597,7 @@ class RobloxManager:
     def get_roblox_packages():
         return ["ywcw.lnu.exhl", "ub.wnjb.bzz", "ixq.vf.jlr", "srl.mvn.gv", "kxm.ak.qyfi", "tk.lisa.cqt", 
                 "jpaclone.anya.lh", "jpaclone.anya.li", "jpaclone.anya.lj", "com.roblox.clienu", "com.roblox.clienv",
-                "com.roblox.clienw", "com.roblox.clienx"
+                "com.roblox.clienw", "com.roblox.clienx", "xtv.popp.kls", "kpl.mlst.nkl", "ply.oudj.tys", "lpo.mksr.wer"
         ]
 
     @staticmethod
